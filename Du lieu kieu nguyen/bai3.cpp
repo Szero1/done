bf335069ff7f3aea6f905566e8d2040dd4318263
Szero1 <tini.vu.0@gmail.com>
2021-06-10 13:08:54 +0700

    update

--- Du lieu kieu nguyen/bai3.cpp
+++ Du lieu kieu nguyen/bai3.cpp
@@ -1,23 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int a,b,c;
-    cin >> a >> b >> c;
-    if (a == c){
-        cout << b << "[cs" << a << "]=" << b << "[cs" << c << "]";
-        return 0;
-    }
-    if (c == 10){
-        int temp = 0;
-        for (int i = b,j = 0;i!=0;i/=10,++j) temp += (i % 10 * pow(a,j));
-        cout << b << "[cs" << a << "]=" << temp << "[cs" << c << "]";
-        return 0;
-    }
+int convert10(const int &a,const string &b){
     int temp = 0;
-    for (int i = b,j = 0;i!=0;i/=10,++j) temp += (i % 10 * pow(a,j));
-    cout << b << "[cs" << a << "]=" << temp << "[cs" << c << "]";
+    if (a == 10) return stoi(b);
+    for (int i = 0;i<b.size();++i) if (b[i] >= 'A' && b[i] <= 'F') temp  += (b[i] - 55) * pow(a,b.size()-i-1);
+    else temp += (b[i] - '0')* pow(a,b.size()-i-1);
+    return temp;
+}
 
+string convert(const int &a,const string &b,const int &c){
+    if (a == c) return b;
+    string temp = "";
+    for (int i = convert10(a,b);i!=0;i/=c){
+        if ((i%c>=10)&&(i%c<=15)) temp = char(i % c + 55) + temp;
+        else temp = char(i % c + '0') + temp;
+    }
+    return temp;
+}
+
+int main()
+{   
+    int a,c;
+    string b;
+    cin >> a >> b >> c;
+    cout << b << "[cs" << a << "]=" << convert(a,b,c) << "[cs" << c << "]";
     return 0;
 }
