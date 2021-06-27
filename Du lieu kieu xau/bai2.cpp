d2ee935e509bf612e84ccdd912252a40b7d7c7b6
Szero1 <tini.vu.0@gmail.com>
2021-06-27 22:41:40 +0700

    Update bai2.cpp

--- Du lieu kieu xau/bai2.cpp
+++ Du lieu kieu xau/bai2.cpp
@@ -6,6 +6,7 @@ int main()
     string s,temp;
     getline(cin,s);
     getline(cin,temp);
+    s = s + ' ';
     map<string,int> A;
     for (int i = s.find(' ',0);i!=-1;i = s.find(' ',0)){
         if (s.front() == ' ') while(s.front() == ' ') s.erase(s.begin());
@@ -14,7 +15,6 @@ int main()
             s.erase(0,i+1);
         }
     }
-    ++A[s];
     int count = 0;
     for (auto &item:A) count += item.second;
     cout << count << "\n" << A[temp]; 
