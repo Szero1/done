f5212365eec9f2a7ea48efbc71c519755b11f368
Szero1 <tini.vu.0@gmail.com>
2021-06-29 22:01:52 +0700

    update

--- Du lieu kieu xau/bai10.cpp
+++ Du lieu kieu xau/bai10.cpp
@@ -12,14 +12,13 @@ int main()
         if (s[k+1] == s[i]) ++k;
         pre[i] = k;
     }
-    /*for (int i = 0,k = -1;t[i];++i){
+    for (int i = 0,k = -1;t[i];++i){
         while (k >= 0 && s[k+1] != t[i]) k = pre[k];
         if (s[k+1] == t[i]) ++k;
         if (s[k+1] == 0){
             cout << i - k + 1 << " ";
             k = pre[k];
         } 
-    }*/
-    for (auto &item:pre) cout << item << " ";
+    }
     return 0;
 }
